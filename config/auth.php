f8d4b500e4780b4be3744236bd1068828644c808
programmarshakil <shakljoka103@gmail.com>
2023-05-06 17:11:56 +0600

    api guard added

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,12 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+
+        'api' => [
+            'driver' => 'jwt',           // before 'jwt' was 'token'
+            'provider' => 'users',
+            'hash' => false,
+        ],
     ],
 
     /*
