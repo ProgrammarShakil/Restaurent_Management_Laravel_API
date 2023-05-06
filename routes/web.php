979e1bf55ac578e40e57ca15ea4ce455345f9399
programmarshakil <shakljoka103@gmail.com>
2023-05-06 14:54:04 +0600

    add user role permission routes

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,16 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
 */
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// **************** user role permission ********************* //
+
+Route::group(['middleware' => ['auth']], function() {
+    Route::resource('roles', RoleController::class);
+    Route::resource('users', UserController::class);
+    // Route::resource('projects', ProjectController::class);
 });
